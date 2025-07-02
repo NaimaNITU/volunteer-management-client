90746b0ec5468343165b1b0c6d197adb4c335bac
Robiul Hasan Raian <toktikproject@gmail.com>
2025-07-02 16:38:07 +0600

    table responsive ok

--- src/pages/VolunteerPosts.tsx
+++ src/pages/VolunteerPosts.tsx
@@ -53,8 +53,9 @@ export default function VolunteerPosts() {
 
   const TableView = () => (
     <div className="bg-white rounded-lg shadow overflow-hidden">
+      {/* Make sure this container can scroll on small screens */}
       <div className="overflow-x-auto">
-        <table className="min-w-full divide-y divide-gray-200">
+        <table className="w-full min-w-[800px] divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
